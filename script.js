32f32f9eef60f4a20c4d1d390cdac6f2fd00dfdc
prtksengar3 <prtksengar3@gmail.com>
2021-03-08 02:12:17 +0530

    sort and clear

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let ulTasks = $('#ulTasks')
 let btnAdd = $('#btnAdd')
 let btnReset = $('#btnReset')
 let inpNewTask = $('#inpNewTask')
+let btnClear = $('#btnClear')
+let btnSort = $('#btnSort')
 
 function addItem(){
     let listItem = $('<li>',{
@@ -15,8 +17,18 @@ function addItem(){
     inpNewTask.val("");
 }
 
+function cleanTasks(){
+    $('#ulTasks .done').remove()
+}
+
+function sortTasks(){
+    $('#ulTasks .done').appendTo(ulTasks)
+}
+
 inpNewTask.keypress((e)=>{
     if(e.which==13) addItem()
 })
 btnAdd.click(addItem)
+btnClear.click(cleanTasks)
+btnSort.click(sortTasks)
 btnReset.click(()=>inpNewTask.val(""))
